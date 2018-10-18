551e07165a8c20effcbfb604137f0c4d6f04a749
Isaac Kwon <isaac-kwon@hotmail.com>
2018-10-18 12:27:24 -0700

    config redux store on client & server

--- server/src/helpers/renderer.js
+++ server/src/helpers/renderer.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Routes from '../client/Routes';
 
-export default req => {
+export default (req, store) => {
   const content = ReactDOMServer.renderToString(
-    <StaticRouter context={{}} location={req.path}>
-      <Routes />
-    </StaticRouter>
+    <Provider store={store}>
+      <StaticRouter context={{}} location={req.path}>
+        <Routes />
+      </StaticRouter>
+    </Provider>
   );
+
   return `
     <html>
       <head>
