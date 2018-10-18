fd36a744feb90a7951096224e5741e78f117e790
isaac1104 <isaac-kwon@hotmail.com>
2018-10-17 20:20:07 -0700

    client bundles refactor

--- server/src/index.js
+++ server/src/index.js
@@ -4,9 +4,20 @@ import ReactDOMServer from 'react-dom/server';
 import Home  from './client/components/Home';
 const app = express();
 
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   const content = ReactDOMServer.renderToString(<Home />);
-  res.send(content);
+  const html = `
+    <html>
+      <head>
+      </head>
+      <body>
+        <div>${content}</div>
+        <script src='bundle.js'></script>
+      </body>
+    </html>
+  `;
+  res.send(html);
 });
 
 app.listen(3000, () => {
