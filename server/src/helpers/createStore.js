379e879979b543ce6acd46973f30eec9c35733f2
Isaac Kwon <isaac-kwon@hotmail.com>
2018-10-22 10:10:22 -0700

    setup server axios instance

--- server/src/helpers/createStore.js
+++ server/src/helpers/createStore.js
@@ -1,8 +1,17 @@
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import axios from 'axios';
 import rootReducer from '../client/reducers';
 
-export default () => {
-  const store = createStore(rootReducer, {}, applyMiddleware(reduxThunk));
+export default req => {
+  const axiosInstance = axios.create({
+    baseURL: 'http://react-ssr-api.herokuapp.com',
+    headers: { cookie: req.get('cookie') || '' }
+  });
+  const store = createStore(
+    rootReducer,
+    {},
+    applyMiddleware(reduxThunk.withExtraArgument(axiosInstance))
+  );
   return store;
 };
