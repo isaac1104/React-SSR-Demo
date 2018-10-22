10d00790936b86ffb4d0074caab0340f5ca211fd
Isaac Kwon <isaac-kwon@hotmail.com>
2018-10-22 13:54:12 -0700

    create NotFoundPage component

--- server/public/bundle.js
+++ server/public/bundle.js
@@ -40165,6 +40165,10 @@ var _UsersListPage = __webpack_require__(468);
 
 var _UsersListPage2 = _interopRequireDefault(_UsersListPage);
 
+var _NotFoundPage = __webpack_require__(472);
+
+var _NotFoundPage2 = _interopRequireDefault(_NotFoundPage);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.default = [_extends({}, _App2.default, {
@@ -40173,7 +40177,7 @@ exports.default = [_extends({}, _App2.default, {
     exact: true
   }), _extends({}, _UsersListPage2.default, {
     path: '/users'
-  })]
+  }), _extends({}, _NotFoundPage2.default)]
 })];
 
 /***/ }),
@@ -40547,6 +40551,35 @@ var authReducer = function authReducer() {
 };
 
 exports.default = authReducer;
+
+/***/ }),
+/* 472 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = __webpack_require__(5);
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var NotFoundPage = function NotFoundPage() {
+  return _react2.default.createElement(
+    'h3',
+    { className: 'center-align' },
+    'Page Not Found'
+  );
+};
+
+exports.default = {
+  component: NotFoundPage
+};
 
 /***/ })
 /******/ ]);
---
+++ server/src/client/pages/NotFoundPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return <h3 className='center-align'>Page Not Found</h3>;
+};
+
+export default {
+  component: NotFoundPage
+};
